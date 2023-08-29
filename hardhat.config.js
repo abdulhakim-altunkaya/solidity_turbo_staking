3cec7309dfa801b0a58a68f5e98e675424c805a7
abdulhakim-altunkaya <altunkaya2@gmail.com>
2023-08-29 15:23:49 +0300

    initial setup finished

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.7",
+  networks: {
+    bnb: {
+      url: process.env.PROVIDER_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    }
+  }
 };
